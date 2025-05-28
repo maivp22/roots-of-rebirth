d22606162f14990d0f4e05a9c2634ad04a7112b2
mailethv <mailethv@uninorte.edu.co>
2025-05-28 16:48:36 -0500

    Cinematica e imagen selector

--- script/mainmenu.gd
+++ script/mainmenu.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 
 func _on_button_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/characterselector.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/cinematica.tscn") # Replace with function body.
 
 
 func _on_button_exit_pressed() -> void:

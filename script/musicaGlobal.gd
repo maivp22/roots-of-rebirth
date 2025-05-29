d8b39b1fa03ce58445a8e2e29fba8dd41c1e5994
maiguelmv <maiguelm@uninorte.edu.co>
2025-05-29 17:52:12 -0500

    inter

--- script/musicaGlobal.gd
+++ script/musicaGlobal.gd
@@ -4,9 +4,11 @@ var music_player: AudioStreamPlayer
 
 func _ready():
 	music_player = AudioStreamPlayer.new()
-	music_player.stream = load("res://musica/musica.mp3")  # Cambia por tu archivo real
+	music_player.stream = load("res://musica/musica.mp3") 
+	if music_player.stream:
+		music_player.stream.loop = true
 	add_child(music_player)
 	music_player.play()
-	
-	
+
+
 	

e377cb2b8ab34e469a6b82e2a6b5853a039f9502
AleSanzC <alejandrasanz@uninorte.edu.co>
2025-05-30 07:32:47 -0500

    Inventario NonStack, Knockback

--- script/inventory_item_ui.gd
+++
@@ -1,9 +0,0 @@
-class_name InventoryItemUI extends Panel
-
-func initialize(texture: CompressedTexture2D, quantity: int):
-	$TextureRect.texture = texture
-	$Label.text = str(quantity)
-	
-
-func set_quantity(quantity: int):
-	$Label.text = str(quantity)
